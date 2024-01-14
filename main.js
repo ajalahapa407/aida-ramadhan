85cae0b33bb2e931f91d4762522ea07d18a6ef07
ajalahapa407 <156569103+ajalahapa407@users.noreply.github.com>
2024-01-14 14:41:21 +0700

    main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function clicks() {
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["Cuma mau bilang", "Selamat Sore Sayangku", "Aku Sayang Banget Sama Kamu", "I Love You Annisa Aulia"],
+        strings: ["apis Cuma mau bilang", "mungkin apis loba membuat kesalahan. loba membuat aida kecewa tapi apis berusaha terus beh bisa nyenangkeun kamu terus", " tapi apis bener bener kok ka kamu","Aku Sayang Banget Sama Kamu", "I Love You more Aida Ramadhan"],
         typeSpeed: 70
     });
 }
