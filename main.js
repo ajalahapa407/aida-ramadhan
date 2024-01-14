7fcf7b1bb472c1709a7d4c4699a17a248bd2502e
ajalahapa407 <156569103+ajalahapa407@users.noreply.github.com>
2024-01-14 14:55:20 +0700

    main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function clicks() {
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["apis Cuma mau bilang", "mungkin apis loba membuat kesalahan. loba membuat aida kecewa tapi apis berusaha terus beh bisa nyenangkeun kamu terus", " tapi apis bener bener kok ka kamu","Aku Sayang Banget Sama Kamu", "I Love You more Aida Ramadhan"],
+        strings: ["apis Cuma mau bilang", "mungkin apis loba membuat kesalahan. loba membuat aida kecewa tapi apis berusaha terus beh bisa nyenangkeun kamu terus", "apis bener bener kok ka kamu","apis Sayang Banget Sama aida","apis nyiuen ieu beh ngabuktikeun ka aida bahwa apis teh bener bener sayang. aida ge jadi orang pertama nu ku apis buat keun ieu ", "I Love You more Aida Ramadhan"],
         typeSpeed: 70
     });
 }
